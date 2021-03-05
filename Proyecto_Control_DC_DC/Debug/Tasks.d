3f56d1005ce08cc6851922e8f82167dd71811538
Sebastian <sebastian.toro.12@sansano.usm.cl>
2021-03-05 12:19:47 -0300

    Version 1.1

---
+++ Proyecto_Control_DC_DC/Debug/Tasks.d
@@ -0,0 +1,133 @@
+# FIXED
+
+Tasks.obj: ../Tasks.cla
+Tasks.obj: ../task_file.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F28x_Project.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_Cla_typedefs.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_device.h
+Tasks.obj: D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/stdbool.h
+Tasks.obj: D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/_ti_config.h
+Tasks.obj: D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/linkage.h
+Tasks.obj: D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/stddef.h
+Tasks.obj: D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/stdint.h
+Tasks.obj: D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/_stdint40.h
+Tasks.obj: D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/sys/stdint.h
+Tasks.obj: D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/sys/cdefs.h
+Tasks.obj: D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/sys/_types.h
+Tasks.obj: D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/machine/_types.h
+Tasks.obj: D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/machine/_stdint.h
+Tasks.obj: D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/sys/_stdint.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_adc.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_analogsubsys.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_cla.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_clb.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_clbxbar.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_cmpss.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_cputimer.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_dac.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_dcsm.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_dma.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_ecap.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_emif.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_epwm.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_epwm_xbar.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_eqep.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_flash.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_gpio.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_i2c.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_input_xbar.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_ipc.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_mcbsp.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_memconfig.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_nmiintrupt.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_output_xbar.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_piectrl.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_pievect.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_sci.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_sdfm.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_spi.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_sysctrl.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_upp.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_xbar.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_xint.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_Examples.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_GlobalPrototypes.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_cputimervars.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_Cla_defines.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_EPwm_defines.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_Adc_defines.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_Emif_defines.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_Gpio_defines.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_I2c_defines.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_Ipc_defines.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_Pie_defines.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_Dma_defines.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_SysCtrl_defines.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_Upp_defines.h
+Tasks.obj: D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_defaultisr.h
+
+../Tasks.cla: 
+../task_file.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F28x_Project.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_Cla_typedefs.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_device.h: 
+D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/stdbool.h: 
+D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/_ti_config.h: 
+D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/linkage.h: 
+D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/stddef.h: 
+D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/stdint.h: 
+D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/_stdint40.h: 
+D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/sys/stdint.h: 
+D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/sys/cdefs.h: 
+D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/sys/_types.h: 
+D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/machine/_types.h: 
+D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/machine/_stdint.h: 
+D:/TI/ccs/tools/compiler/ti-cgt-c2000_20.2.4.LTS/include/sys/_stdint.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_adc.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_analogsubsys.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_cla.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_clb.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_clbxbar.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_cmpss.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_cputimer.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_dac.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_dcsm.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_dma.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_ecap.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_emif.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_epwm.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_epwm_xbar.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_eqep.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_flash.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_gpio.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_i2c.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_input_xbar.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_ipc.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_mcbsp.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_memconfig.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_nmiintrupt.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_output_xbar.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_piectrl.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_pievect.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_sci.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_sdfm.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_spi.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_sysctrl.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_upp.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_xbar.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/include/F2837xD_xint.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_Examples.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_GlobalPrototypes.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_cputimervars.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_Cla_defines.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_EPwm_defines.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_Adc_defines.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_Emif_defines.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_Gpio_defines.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_I2c_defines.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_Ipc_defines.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_Pie_defines.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_Dma_defines.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_SysCtrl_defines.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_Upp_defines.h: 
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/include/F2837xD_defaultisr.h: 
---
+++ Proyecto_Control_DC_DC/Debug/subdir_vars.mk
@@ -0,0 +1,109 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/cmd/F2837xD_Headers_nonBIOS_cpu1.cmd \
+../f2837x_cla_lnk.cmd 
+
+CLA_SRCS += \
+../Tasks.cla 
+
+ASM_SRCS += \
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/source/F2837xD_CodeStartBranch.asm \
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/source/F2837xD_usDelay.asm 
+
+C_SRCS += \
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/source/F2837xD_Adc.c \
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/source/F2837xD_DefaultISR.c \
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/source/F2837xD_GlobalVariableDefs.c \
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/source/F2837xD_Gpio.c \
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/source/F2837xD_Ipc.c \
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/source/F2837xD_PieCtrl.c \
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/source/F2837xD_PieVect.c \
+D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/source/F2837xD_SysCtrl.c \
+../main.c 
+
+CLA_DEPS += \
+./Tasks.d 
+
+C_DEPS += \
+./F2837xD_Adc.d \
+./F2837xD_DefaultISR.d \
+./F2837xD_GlobalVariableDefs.d \
+./F2837xD_Gpio.d \
+./F2837xD_Ipc.d \
+./F2837xD_PieCtrl.d \
+./F2837xD_PieVect.d \
+./F2837xD_SysCtrl.d \
+./main.d 
+
+OBJS += \
+./F2837xD_Adc.obj \
+./F2837xD_CodeStartBranch.obj \
+./F2837xD_DefaultISR.obj \
+./F2837xD_GlobalVariableDefs.obj \
+./F2837xD_Gpio.obj \
+./F2837xD_Ipc.obj \
+./F2837xD_PieCtrl.obj \
+./F2837xD_PieVect.obj \
+./F2837xD_SysCtrl.obj \
+./F2837xD_usDelay.obj \
+./Tasks.obj \
+./main.obj 
+
+ASM_DEPS += \
+./F2837xD_CodeStartBranch.d \
+./F2837xD_usDelay.d 
+
+OBJS__QUOTED += \
+"F2837xD_Adc.obj" \
+"F2837xD_CodeStartBranch.obj" \
+"F2837xD_DefaultISR.obj" \
+"F2837xD_GlobalVariableDefs.obj" \
+"F2837xD_Gpio.obj" \
+"F2837xD_Ipc.obj" \
+"F2837xD_PieCtrl.obj" \
+"F2837xD_PieVect.obj" \
+"F2837xD_SysCtrl.obj" \
+"F2837xD_usDelay.obj" \
+"Tasks.obj" \
+"main.obj" 
+
+C_DEPS__QUOTED += \
+"F2837xD_Adc.d" \
+"F2837xD_DefaultISR.d" \
+"F2837xD_GlobalVariableDefs.d" \
+"F2837xD_Gpio.d" \
+"F2837xD_Ipc.d" \
+"F2837xD_PieCtrl.d" \
+"F2837xD_PieVect.d" \
+"F2837xD_SysCtrl.d" \
+"main.d" 
+
+CLA_DEPS__QUOTED += \
+"Tasks.d" 
+
+ASM_DEPS__QUOTED += \
+"F2837xD_CodeStartBranch.d" \
+"F2837xD_usDelay.d" 
+
+C_SRCS__QUOTED += \
+"D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/source/F2837xD_Adc.c" \
+"D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/source/F2837xD_DefaultISR.c" \
+"D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/headers/source/F2837xD_GlobalVariableDefs.c" \
+"D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/source/F2837xD_Gpio.c" \
+"D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/source/F2837xD_Ipc.c" \
+"D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/source/F2837xD_PieCtrl.c" \
+"D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/source/F2837xD_PieVect.c" \
+"D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/source/F2837xD_SysCtrl.c" \
+"../main.c" 
+
+ASM_SRCS__QUOTED += \
+"D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/source/F2837xD_CodeStartBranch.asm" \
+"D:/TI/C2000Ware_3_04_00_00_Software/device_support/f2837xd/common/source/F2837xD_usDelay.asm" 
+
+
